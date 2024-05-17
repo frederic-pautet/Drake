0f09b2aeb845a776c803bd6b021ff6d82efa9754
frederic-pautet <128693160+frederic-pautet@users.noreply.github.com>
2024-05-17 17:22:07 +0200

    Update main.py

---
+++ code/main.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+
+from Interfaces import ClassicMenu
+from Interfaces import TriviaMenu
+from Interfaces import MultiplayerMenu
+from Interfaces import HighscoresWindow
+
+def create_main_menu():
+    
+    def start_game(mode):
+        print(f"Starting {mode} mode...")  # Placeholder for actual game logic
+    
+    def show_highscores():
+        print("Showing highscores...")  # Placeholder for actual highscores display logic
+    
+    # Create the main window
+    root = tk.Tk()
+    root.title("Snake Game Menu")
+    root.attributes("-fullscreen", True)  #full screen
+    
+    # Function to create buttons
+    def create_button(text, command):
+        button = tk.Button(root, text=text, command=lambda: command(root), width=20)
+        button.pack(pady=5)
+
+    tk.Label(root, text="Drake The Snake Game:").pack(pady=5)
+    # Buttons for different game modes
+    create_button("Classic Mode", ClassicMenu.main)
+    create_button("Trivia Mode", TriviaMenu.main)
+    create_button("Multiplayer Mode", MultiplayerMenu.main)
+    
+    # Button to view highscores
+    create_button("Highscores", HighscoresWindow.main)
+
+    #Button to lave the game
+    create_button("Leave Game", leave_game)
+    
+    # Run the tkinter event loop
+    root.mainloop()
+
+
+def leave_game(root): #to close the program
+    root.destroy()
+    exit()
+
+
+if __name__ == "__main__":
+    create_main_menu()
